3b8b456d9df5123918286af8875a4c5635db6ea4
shivam11sae <shivamsharmading@gmail.com>
2025-07-13 11:21:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ import "dotenv/config";
 
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb://localhost:27017/bookstore";
+export const mongoDBURL = "mongodb+srv://bookstore:shivam%40123@cluster0.ee45khs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
